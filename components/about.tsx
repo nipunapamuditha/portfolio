b4cc2c6e8f3cff99146b8888ba40b23936d7de78
Nipuna <nipunapamuditha234@gmail.com>
2025-05-22 00:53:33 -0400

    da

--- components/about.tsx
+++ components/about.tsx
@@ -8,12 +8,16 @@ export default function About() {
           <h2 className="text-3xl font-bold text-center text-gray-900 dark:text-white mb-8">About Me</h2>
           <Card>
           <CardContent className="pt-6">
-          <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
-                I'm a dedicated Site Reliability Engineer (SRE) based in Toronto, ON, with a strong focus on cloud infrastructure, automation, and technical support. My expertise spans across various cloud platforms and technologies, ensuring robust and efficient system operations.
+                <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
+                I'm a Site Reliability Engineer based in Toronto, ON, passionate about building and maintaining reliable, scalable systems. My work mainly revolves around cloud infrastructure, automation, and solving technical challenges to keep systems running smoothly.
               </p>
 <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
-  I hold AWS SAA, AWS SysOps, and AZ-104 certifications. With 2+ years as a Site Reliability Engineer at Mad Mobile, one of the largest POS service providers in the U.S., I focused on enhancing system reliability, investigating and patching software bugs, managing incidents, and optimizing performance. In my free time, I build projects like Substack Co-Pilot (an AI news aggregator with Large Language Model-powered audio briefings) and contribute to open-source tools like Jellyfin, developing a containerized torrent-based backend solution for media servers.
+ I'm certified in AWS (SAA and SysOps) and Azure (AZ-104), and I bring over two years of experience from my time at Mad Mobile, a leading POS provider in the U.S. There, I worked on investigating and patching software bugs, managing incidents, and optimizing performance. 
 </p>
+
+    <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
+                Outside of work, I enjoy building personal projects like Substack Co-Pilot, and contributing to open-source products like Jellyfin, a media server—where I’ve developed a containerized, torrent-based backend solution to streamline media delivery.
+              </p>
     
 </CardContent>
 
